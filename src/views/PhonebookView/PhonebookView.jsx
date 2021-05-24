52253bf02057a7c4f20dc267b421738038e51f7f
Sergey Glazyrin <glazyrinsb@gmail.com>
2021-05-25 00:17:30 +0300

    update

--- src/views/PhonebookView/PhonebookView.jsx
+++ src/views/PhonebookView/PhonebookView.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { contactsOperations, contactsSelectors } from '../../redux/contacts';
 
